9ea4a6ccc3fbc4d6a241889546f5860b1070e4d6
euphoriiah-dev <euphoriiah.dev@gmail.com>
2024-01-30 19:09:25 +0100

    navigation settings

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,30 @@ import '../style/components/navbar.css'
 
 function Navbar () {
     return(
-        <div className="navbar-container">
-            
-        </div>
+        <nav className="main-nav">
+    <ul>
+        <li className="first-nav-li"><a href="#" id="p1-link">Nos produits</a>
+            <ul class="sub-menu">
+                <li><a href="#">Fruits et légumes</a></li>
+                <li><a href="#">Épicerie sucrée</a></li>
+                <li><a href="#">Épicerie salée</a></li>
+                <li><a href="#">Produits frais</a></li>
+                <li><a href="#">Boissons</a></li>
+                <li><a href="#">Hygiène et beauté</a></li>
+                <li><a href="#">Nouveautés</a></li>
+            </ul>
+        </li>
+        <li className="first-nav-li">
+            <a href="#">Nos producteurs</a>
+        </li>
+        <li className="first-nav-li">
+            <a href="#">Nos magasins</a>
+        </li>
+        <li className="first-nav-li">
+            <a href="#">A propos de nous</a>
+        </li>
+    </ul>
+        </nav>
     )
 }
 
